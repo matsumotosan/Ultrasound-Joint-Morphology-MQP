978f8b1c51a1e9e93b6dfd8d6cdd0693fd47f525
rsheidt <rsheidt@wpi.edu>
2019-01-14 16:34:57 -0500

    current filter; still lowpass but works

--- gyroviz.m
+++ gyroviz.m
@@ -176,35 +176,46 @@ ylabel('Degrees (\circ)');
 legend('Yaw', 'Pitch', 'Roll');
 
 %% Apply filter
-fs = 1 / mean(diff(t));         % sampling frequency
-% acc_fil = lowpass(acc,1,fs);   % conventional low pass filter
+fs = 1 / mean(diff(t));                     % sampling frequency
+% acc_fil = lowpass(acc,1,fs);              % conventional low pass filter
 
 d = designfilt('lowpassfir', ...
     'PassbandFrequency',0.03,'StopbandFrequency',0.08, ...
     'PassbandRipple',0.5,'StopbandAttenuation',65, ...
     'DesignMethod','equiripple');
 
+% % FFT of the acc data
+% T = 1/fs;             % Sampling period       
+% L = length(acc);      % Length of signal
+% t = (0:L-1)*T;        % Time vector
+% Y = fft(acc);
+% P2 = abs(Y/L);
+% P1 = P2(1:L/2+1);
+% P1(2:end-1) = 2*P1(2:end-1);
+% f = fs*(0:(L/2))/L;
+% plot(f,P1) 
+% title('Single-Sided Amplitude Spectrum of Acc(t)')
+% xlabel('f (Hz)')
+% ylabel('|P1(f)|')
+     
 acc_fil = filtfilt(d,acc);   % zero-phase digital filter
-
-% [r c] = size(acc_fil);
-
 accel_zero = zeros(1,length(acc_fil));
 
 figure; hold on
 subplot(3,1,1)
-plot(t,acc_fil(:,1),t,acc(:,1),t,accel_zero,'k');
+plot(t,acc_fil(:,1),t,acc(:,1),t,accel_zero);
 title('Angular Displacement Yaw (x)')
 xlabel('Time (s)');
 ylabel('Acceleration (mm/s^2)');
 
 subplot(3,1,2)
-plot(t,acc_fil(:,2),t,acc(:,2),t,accel_zero,'k');
+plot(t,acc_fil(:,2),t,acc(:,2),t,accel_zero);
 title('Angular Displacement Pitch (y)')
 xlabel('Time (s)');
 ylabel('Acceleration (mm/s^2)');
 
 subplot(3,1,3)
-plot(t,acc_fil(:,3),t,acc(:,3),t,accel_zero,'k');
+plot(t,acc_fil(:,3),t,acc(:,3),t,accel_zero);
 title('Angular Displacement Roll (z)')
 xlabel('Time (s)');
 ylabel('Acceleration (mm/s^2)');
