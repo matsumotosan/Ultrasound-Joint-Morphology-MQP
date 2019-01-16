a73f77f99a2c74c43723c52ca950ec115b612e2d
Shion <>
2019-01-16 10:16:02 -0500

    Add documentation

--- imaqtool_vidaq.m
+++ imaqtool_vidaq.m
@@ -1,27 +1,28 @@
-%% Imaqtool Use without Toolbox Attempt
+%% Acquire US video through Image Acquisition Toolbox programmatically
 % Plug in Arduino and Hauppage before opening MATLAB
+%
+% Requires Image Acquisition Toolbox
 
+close all; clear; clc;
+
+%% Acquire images
 % acquire Device Info for line 9
 % info = imaqhwinfo('winvideo');
 % ID = info.DeviceInfo(1).DefaultFormat;
 
-close all; clear all; clc;
-
-% Create video input object. 
 vid = videoinput('winvideo',1,'UYVY_720x480');
 
 triggerconfig(vid, 'Manual');
 vid.FramesPerTrigger = 1;
 
 start(vid)
-
 vid.TriggerRepeat = 9;
 
-tic
+tic     % start timer
 for i = 1:10
-frame{i} = trigger(vid);
-toc
+    frame{i} = trigger(vid);    % acquire frame
+    toc     % time of each acquisition
 end
 
-
 stop(vid)
+
