2db210d72db85cd21341ed73c2d38b0019497177
rsheidt <rsheidt@wpi.edu>
2019-01-16 12:06:25 -0500

    working matlab script for image acquisition. note: need to close everything before running for optimal speed.

--- imaqtool_vidaq.m
+++ imaqtool_vidaq.m
@@ -1,9 +1,13 @@
 %% Acquire US video through Image Acquisition Toolbox programmatically
 % Plug in Arduino and Hauppage before opening MATLAB
-%
+% Best when running alone on computer
 % Requires Image Acquisition Toolbox
 
 close all; clear; clc;
+
+if exist('vid')
+    stop(vid);
+end
 
 %% Acquire images
 % acquire Device Info for line 9
@@ -11,18 +15,29 @@ close all; clear; clc;
 % ID = info.DeviceInfo(1).DefaultFormat;
 
 vid = videoinput('winvideo',1,'UYVY_720x480');
+preview(vid);
 
 triggerconfig(vid, 'Manual');
-vid.FramesPerTrigger = 1;
 
+%% Gather Frames
 start(vid)
-vid.TriggerRepeat = 9;
+tic
+for i = 1:4000
+    
+    frames{i} = getsnapshot(vid);
+    t(i) = toc;
 
-tic     % start timer
-for i = 1:10
-    frame{i} = trigger(vid);    % acquire frame
-    toc     % time of each acquisition
+    % potential timestamp tag
+    %     if i == 1
+    %         fprintf('Start time of data acquistion: %s \n', datestr(now,'mm-dd-yyyy MM.SS.FFF'));
+    %     end
 end
 
+%% Close Video
 stop(vid)
+delete(vid)
 
+%% Calculate fs
+
+t = t';
+fs = 1/mean(diff(t));
