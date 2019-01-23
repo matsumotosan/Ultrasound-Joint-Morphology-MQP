460e74495cc4e69162dad8e6ececd01d3568a2eb
Shion <>
2019-01-23 16:01:22 -0500

    Outline reconstruction procedure and necessary functions.

--- Reconstruction/reconstruct.m
+++ Reconstruction/reconstruct.m
@@ -1,44 +1,91 @@
+% Script to complete 3D reconstruction (pixel nearest neighbor algorithm)
+% of image based on 2D ultrasound scans with pose data obtained by an 
+% inertial measurement unit. The overall process is as follows:
+%
+% 0) Enter names of necessary files
+% 1) Calculate probe pose from IMU data
+% 2) US video pre-processing (cropping, pose tagging)
+% 3) Calculate bin dimensions and transform coordinates
+% 4) Reconstruction (distribution step)
+% 5) Reconstruction (hole-filling step)
+% 6) Visualization
+%
+% Requires following MATLAB Toolboxes:
+%   - Image Processing
+%
+% Check toolboxes needed for this script:
+% [fList,pList] = matlab.codetools.requiredFilesAndProducts('reconstruct.m');
+
 clear; clc; close all
 
-%% IMU data
-% IMU reading and settings
-run1_imu = cell2mat(struct2cell(load('recon_run1.mat')));
-run2_imu = cell2mat(struct2cell(load('recon_run2.mat')));
+% Note:
+% Try to vectorize as much of the code as possible
+% The script is going to be handling a lot of data, so either use MATLAB's
+% vectorization or create functions to minimize the number of variables
+% needed in the workspace.
 
-% Sampling rate
-typr = cell2mat(struct2cell(load('typr.mat')));
-rate = mean(diff(typr(:,1)));
+%% 0) Enter names of necessary files - comment during development
+% imu_file = '';  % raw IMU data
+% vid_file = '';  % raw US data
 
-% Plot IMU data
-t = 0:rate:rate * length(run1_imu);
-t = linspace(0,100,length(run1_imu));
-plot(t,run1_imu(:,1),t,run1_imu(:,2),t,run1_imu(:,3))
-legend('Yaw','Pitch','Roll')
-xlabel('t (s)')
-ylabel('Degrees (\circ)')
+%% 1) Calculate probe pose from IMU data
+% Load IMU data
+imu_file = 'typr.mat';  % Enter name of IMU data file
+imu_data = cell2mat(struct2cell(load(which(imu_file))));
 
-%% Video data
-% Load video
-dir = '/Users/Shion/Dropbox/MQP-US Media/12-11 test w imu';  % Shion's directory
-vid_name = 'recon_run2.mp4';
+% Filter pose data - low pass filter and moving window
+% (Filtering function) - ROSIE
 
-v = VideoReader(strcat(dir,'/',vid_name));
-% noFrames = v.NumberOfFrames;    % number of frames
-vDuration = v.Duration;
 
-% Read frames
+% Calculate displacement from linear acceleration using composite
+% trapezoidal numerical integration scheme
+[disp,~] = calcDisp(acc,t);
+
+
+%% 2) US video pre-processing
+vid_file = '';  % Enter name of US video file
+vid = VideoReader(vid_file);
+
+% Calculate dimension of pixel and cropping window in mm - ????
+rect = [];      % cropping window
+scale = [];     % mm / pixel
+
+% Convert all frames to grayscale and crop
 frames = {};
-i = 1;
-rect = [30, 87, 510, 250];
-
-while hasFrame(v)
-    frames{i} = imcrop(rgb2gray(readFrame(v)),rect);
-%     imshow(frame,'Parent',currAxes);
-%     currAxes.Visible = 'off';
-%     pause(0.01);
-    i = i + 1;
+while hasFrame(vid)
+    frames{end + 1} = im2double(imcrop(rgb2gray(readFrame(vid)),rect));
 end
 
-%% Synchrnoize IMU and video
+% Tag frames with pose data - ????
+% (Frame tagging function - possibly just interpolate from disp)
+
+
+%% 3) Calculate bin dimensions and transform coordinates
+% Transform IMU pose data to global coordinates - ????
+
+
+% Calculate limits of bin - ????
+
+
+% Initialze bin for distribution step
+bin = zeros(300,600,100);
+
+%% 4) Reconstruction (distribution step) - SHION
+% Fill voxels - PNN algorithm
+for i = 1:length(frames)
+    bin = fillbin(frames{i},pose(i,:),bin);
+    fprintf('(%d/5) frames completed ...\n',i);
+end
+
+
+%% 5) Reconstruction (hole-filling step)
+bin = fillhole(bin);
+
+
+%% 6) Visualization
+
+
+
+
 
 
