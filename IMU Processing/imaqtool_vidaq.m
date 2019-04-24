52b212dbe96837e6c8bb15616915dcd9cdb02b59
rsheidt <rsheidt@wpi.edu>
2019-04-24 19:12:39 -0400

    Cleaning up the IMU filtering code and the imaqtool_vidaq code

--- IMU Processing/imaqtool_vidaq.m
+++ IMU Processing/imaqtool_vidaq.m
@@ -1,22 +1,35 @@
-%% Acquire US video through Image Acquisition Toolbox programmatically
+%% Acquire US video through Image Acquisition Toolbox
 % Plug in Arduino and Hauppage before opening MATLAB
-% Best when running alone on computer
-% Requires Image Acquisition Toolbox
+% If you encounter any issues unplug the USB for Hauppauge, close MATLAB,
+% plug Hauppauge back in and then open a new MATLAB session.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Note: This code requires Image Acquisition Toolbox
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% For use of the GUI associated with the Image Acq. Toolbox: type the
+% following command into the command window:
+% imaqtool
+
+%% Reset the MATLAB session
 close all; clear; clc;
 
+% delete video object if it exists
 if exist('vid')
     stop(vid);
 end
 
 %% Acquire images
-% acquire Device Info 1for line 17
+
+% % acquire Device Info 1for line 17
 % info = imaqhwinfo('winvideo');
 % ID = info.DeviceInfo(1).DefaultFormat;
 
-vid = videoinput('winvideo',1,'UYVY_720x480');
+%create video object & preview
+vid = videoinput('winvideo',1,'UYVY_720x480'); %inputs may change per computer
 preview(vid);
 
+%allow for manual trigger
 triggerconfig(vid, 'Manual');
 
 %% Gather Frames
@@ -25,10 +38,16 @@ start(vid)
 %change frame num
 frame_num = input('How many frames do you want? [#]  '); 
 
+%begin timer
 tic
+
+%iterate 'getsnapshot' for number of desired frames
 for i = 1:frame_num
     
+    %save frame
     frames{i} = getsnapshot(vid);
+    
+    %store time per frame
     t(i) = toc;
 
     % potential timestamp tag
@@ -37,12 +56,14 @@ for i = 1:frame_num
     %     end
 end
 
-
 %% Close Video
 stop(vid)
 delete(vid)
 
-%% Calculate fs
+%% Calculate sampling frequency (fs)
 
+%reconfigure time
 t = t';
+
+%calculate sampling frequency
 fs = 1/mean(diff(t));
--- medianfiltertesting.m
+++ medianfiltertesting.m
@@ -1,8 +1,12 @@
-%% File to check working filtering
+%% IMU Global Positioning Data Processing Code
+% This code tests the accuracy of the IMU's global positioning skills. It
+% uses the calcDisp function written to calculate displacement from the
+% provided acceleration data and a custom-built lowpass filter.
 
 close all; clear all; clc;
 
 %% Load relevant pose files
+
 load('do_nothing.mat'); %newest no movement file
 %load('square_test.mat'); %start and end points are same, square trial
 %load('line_test.mat'); %line test
@@ -18,40 +22,43 @@ t = pose(2:end,1)/1000;     %extract time data & convert to ms
 %% Calculate displacement (x,y,z)
 postDisp = calcDisp(acc,t); %calculate pose data from calcDisp function
 
-% %% Plot original data
-% % Plot displacement in x,y,z
-% figure; hold on
-% subplot(3,1,1)  % acceleration plot
-% plot(t, acc(:,1), t, acc(:,2), t, acc(:,3)); grid on
-% title('Original Acceleration Plot')
-% xlabel('Time (s)')
-% ylabel('Acceleration (mm/s^2)');
-% legend('a_x','a_y','a_z');
-% 
-% subplot(3,1,2)  % displacement plot
-% plot(t, postDisp(1,:)', t, postDisp(2,:)', t, postDisp(3,:)'); grid on
-% title('Original Displacement Plot')
-% xlabel('Time (s)')
-% ylabel('Displacement (mm)');
-% legend('d_x','d_y','d_z');
-% 
-% subplot(3,1,3)  % angular displacement plot
-% plot(t, ypr(1,:), t, ypr(2,:), t, ypr(3,:)); grid on
-% title('Original Angular Displacement Plot')
-% xlabel('Time (s)');
-% ylabel('Angular Displacement (\circ)');
-% legend('Yaw', 'Pitch', 'Roll');
+%% Plot original data
+% Plot displacement in x,y,z
+figure; hold on
+subplot(3,1,1)  % acceleration plot
+plot(t, acc(:,1), t, acc(:,2), t, acc(:,3)); grid on
+title('Original Acceleration Plot')
+xlabel('Time (s)')
+ylabel('Acceleration (mm/s^2)');
+legend('a_x','a_y','a_z');
+
+subplot(3,1,2)  % displacement plot
+plot(t, postDisp(1,:)', t, postDisp(2,:)', t, postDisp(3,:)'); grid on
+title('Original Displacement Plot')
+xlabel('Time (s)')
+ylabel('Displacement (mm)');
+legend('d_x','d_y','d_z');
+
+subplot(3,1,3)  % angular displacement plot
+plot(t, ypr(1,:), t, ypr(2,:), t, ypr(3,:)); grid on
+title('Original Angular Displacement Plot')
+xlabel('Time (s)');
+ylabel('Angular Displacement (\circ)');
+legend('Yaw', 'Pitch', 'Roll');
 
 %% Calculate Sample Frequency
 fs = 1 / mean(diff(t));             % sampling frequency
 
 %% Median Filter
 acc_fil = medfilt1(acc,10);        % median filt of acc data over 5 sample window
+
+%% Conventional Low-pass Filter
 % acc_fil = lowpass(acc,1,fs);      % conventional low pass filter
-% acc_fil = lowpassfilt(acc);       % low pass on median filtered data
+
+%% Custom Low-pass filter
+% acc_fil = lowpassfilt(acc);       % apply custom low-pass filter
 
 %% Butterworth Band-Pass Filtering
-
 order = 2;     %order of the filter
 fcutlow=1;     %low cut frequency in Hz
 fcuthigh=5;   %high cut frequency in Hz
@@ -59,19 +66,10 @@ fcuthigh=5;   %high cut frequency in Hz
 [b,a]=butter(order,[fcutlow,fcuthigh]/(fs/2),'bandpass');
 bp_acc_fil = filtfilt(b, a, acc);
 
-%% Kalman Filter
-
-% [kest,L,P] = kalman(sys,Qn,Rn,Nn)
-% [kest,L,P] = kalman(sys,Qn,Rn,Nn,sensors,known)
-% [kest,L,P,M,Z] = kalman(sys,Qn,Rn,...,type)
-
 %% Create expected plot for do nothing case
-
 a0 = zeros(length(acc_fil),3); %to compare correct '0' in plot
 
-
 %% Plot filtered data
-green = [0 0.5 0];
 
 figure; hold on
 ax = subplot(3,1,1);
@@ -120,5 +118,4 @@ legend('d_x','d_y','d_z');
 %% Calc error
 
 avg_disp_error = mean(abs(disp_fil(:,end)));
-
 fprintf('Averaged end displacement error of %.2f mm\n',avg_disp_error);
